32d88c3a4d9a5a79af1c13563a07152f72300587
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2022-12-08 00:00:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -101,27 +101,10 @@ function show(shown, hidden) {
       
       Instascan.Camera.getCameras()
         .then(function (cameras) {
-          if (cameras.length > 0) {
+          
             scanner.start(cameras[cameras.length - 1]);
-            $('[name="options"]').on("change", function () {
-              if ($(this).val() == 1) {
-                if (cameras[0] != "") {
-                  scanner.start(cameras[0]);
-                } else {
-                  alert("No Front camera found!");
-                }
-              } else if ($(this).val() == 2) {
-                if (cameras[1] != "") {
-                  scanner.start(cameras[1]);
-                } else {
-                  alert("No Back camera found!");
-                }
-              }
-            });
-          } else {
-            console.error("No cameras found.");
-            alert("No cameras found.");
-          }
+           
+      
         })
         .catch(function (e) {
           console.error(e);
